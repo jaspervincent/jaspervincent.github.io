c224f0a01955e71ee1b6c8ea163ae4574bae2f30
Jasper Vincent <xcwhome@163.com>
2026-01-24 10:42:25 +0800

    up

--- static/aandds.com/js/mathjax.js
+++ static/aandds.com/js/mathjax.js
@@ -1,4 +1,5 @@
 var newScript = document.createElement('script');
 newScript.type = 'text/javascript';
-newScript.src = 'https://unpkg.com/mathjax@3/es5/tex-mml-chtml.js';
+// newScript.src = 'https://unpkg.com/mathjax@3/es5/tex-mml-chtml.js';
+newScript.src = 'https://xuchangwei.com/static/cdn.jsdelivr.net/js/tex-mml-chtml.js';
 document.getElementsByTagName('head')[0].appendChild(newScript);
